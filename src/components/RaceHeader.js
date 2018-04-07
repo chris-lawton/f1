fcd969e86394b4d25d5508caabc26ccf87fb6166
Chris Lawton <chris.lawton@torchbox.com>
2018-04-07 22:29:35 +0100

    remove parent div from season

--- src/components/RaceHeader.js
+++ src/components/RaceHeader.js
@@ -3,14 +3,12 @@ import React from 'react';
 class RaceHeader extends React.Component {
     render() {
         return (
-            <div>
-                <div className="wrapper wrapper--race-header">
-                    <div className={`flag flag--${this.props.circuit.Location.country.toLowerCase()}`} />
-                    <div>
-                        <h2>{this.props.circuit.circuitName}</h2>
-                        <h3>{this.props.circuit.Location.country}</h3>
-                        <h4>{this.props.circuit.Location.locality}</h4>
-                    </div>
+            <div className="wrapper wrapper--race-header">
+                <div className={`flag flag--${this.props.circuit.Location.country.toLowerCase()}`} />
+                <div>
+                    <h2>{this.props.circuit.circuitName}</h2>
+                    <h3>{this.props.circuit.Location.country}</h3>
+                    <h4>{this.props.circuit.Location.locality}</h4>
                 </div>
             </div>
         );
