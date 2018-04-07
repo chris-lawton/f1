62c2b8374816947c4f37145f9f99b32fb6b4e2cb
Chris Lawton <chris.lawton@torchbox.com>
2018-04-07 21:28:39 +0100

    work out days until lights out for future races

--- src/components/FutureRace.js
+++ src/components/FutureRace.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { daysUntilLightsOut } from './../helpers';
 
 class FutureRace extends React.Component {
+    constructor(){
+        super();
+
+        this.state = {
+            daysUntilRace: 0,
+            isLoading: true
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            daysUntilRace: daysUntilLightsOut(this.props.raceDate),
+            isLoading: false
+        })
+    }
+
     render(){
+        if (this.state.isLoading) {
+            return null;
+        }
+
         return(
             <div>
-                <div>Future race</div>
+                <h1>{this.state.daysUntilRace} days until lights out!</h1>
             </div>
         );
     }
--- src/helpers.js
+++ src/helpers.js
@@ -16,3 +16,21 @@ export function compareDate(date) {
 export function isDateBeforeToday(date) {
     return new Date(date.toString()) < new Date(new Date().toString());
 }
+
+export function daysUntilLightsOut(date) {
+    const raceDate = new Date(date);
+    const today = new Date();
+
+    // calculate milli-seconds per day
+    const msPerDay = 1000 * 60 * 60 * 24;
+
+    // discard the time and time-zone information
+    const utc1 = Date.UTC(raceDate.getFullYear(), raceDate.getMonth(), raceDate.getDate());
+    const utc2 = Date.UTC(today.getFullYear(), today.getMonth(), today.getDate());
+
+    // work out the difference
+    const difference = Math.floor((utc2 - utc1) / msPerDay);
+
+    // return a positive number
+    return Math.abs(difference);
+}
